6916cf86304cb8e6af703e0f9e9d51dd58335b4c
Jake <jacobbritten@outlook.com>
2026-05-14 08:22:23 -0400

    Install Vercel web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import SRTFixer from "./components/SRTFixer.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SRTFixer />
+    <Analytics />
   </React.StrictMode>
 );
